061ef29ed42b47f66e5f90dcd20f7167463df786
EveryWareDeveloper <82247880+EveryWareDeveloper@users.noreply.github.com>
2022-01-05 17:38:42 +0900

    스프링부트 프로젝트 시작!

---
+++ usedtrade/src/test/java/com/coll/UsedtradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedtradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
