fb214aa1b223837e0dd5c0acb2d2d3ed3354f793
godalon <hamk1@naver.com>
2022-01-06 21:03:03 +0900

    9시2분

--- usedtrade/src/main/java/com/coll/controller/UserController.java
+++ usedtrade/src/main/java/com/coll/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.coll.Controller;
+package com.coll.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
